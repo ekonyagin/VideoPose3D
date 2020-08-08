d315df453d81dadf6a92df0c9a714d8465298e0e
Michael Vasilkovsky <waytobehigh@gmail.com>
2020-08-08 12:46:12 +0300

    feat: add inference script

---
+++ infer.py
@@ -0,0 +1,142 @@
+# Copyright (c) 2018-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import numpy as np
+
+from common.arguments import parse_args
+import torch
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import os
+import sys
+import errno
+
+from common.camera import *
+from common.model import *
+from common.loss import *
+from common.generators import ChunkedGenerator, UnchunkedGenerator
+from time import time
+from common.utils import deterministic_random
+
+args = parse_args()
+print(args)
+
+input_keypoints = np.load(args.dataset)
+
+filter_widths = [int(x) for x in args.architecture.split(',')]
+
+kps_left, kps_right = [1, 3, 5, 7, 9, 11, 13, 15], [2, 4, 6, 8, 10, 12, 14, 16]
+joints_left, joints_right = [4, 5, 6, 11, 12, 13], [1, 2, 3, 14, 15, 16]
+
+model_pos = TemporalModel(input_keypoints.shape[-2], input_keypoints.shape[-1], input_keypoints.shape[-2],
+                          filter_widths=filter_widths, causal=args.causal, dropout=args.dropout, channels=args.channels,
+                          dense=args.dense)
+
+receptive_field = model_pos.receptive_field()
+print('INFO: Receptive field: {} frames'.format(receptive_field))
+pad = (receptive_field - 1) // 2  # Padding on each side
+if args.causal:
+    print('INFO: Using causal convolutions')
+    causal_shift = pad
+else:
+    causal_shift = 0
+
+model_params = 0
+for parameter in model_pos.parameters():
+    model_params += parameter.numel()
+print('INFO: Trainable parameter count:', model_params)
+
+if torch.cuda.is_available():
+    model_pos = model_pos.cuda()
+
+if args.resume or args.evaluate:
+    chk_filename = os.path.join(args.checkpoint, args.resume if args.resume else args.evaluate)
+    print('Loading checkpoint', chk_filename)
+    checkpoint = torch.load(chk_filename, map_location=lambda storage, loc: storage)
+    print('This model was trained for {} epochs'.format(checkpoint['epoch']))
+    model_pos.load_state_dict(checkpoint['model_pos'])
+
+
+# Evaluate
+def evaluate(test_generator, action=None, return_predictions=False):
+    epoch_loss_3d_pos = 0
+    epoch_loss_3d_pos_procrustes = 0
+    epoch_loss_3d_pos_scale = 0
+    epoch_loss_3d_vel = 0
+    with torch.no_grad():
+        model_pos.eval()
+        N = 0
+        for _, batch, batch_2d in test_generator.next_epoch():
+            inputs_2d = torch.from_numpy(batch_2d.astype('float32'))
+            if torch.cuda.is_available():
+                inputs_2d = inputs_2d.cuda()
+
+            # Positional model
+            predicted_3d_pos = model_pos(inputs_2d)
+
+            # Test-time augmentation (if enabled)
+            if test_generator.augment_enabled():
+                # Undo flipping and take average with non-flipped version
+                predicted_3d_pos[1, :, :, 0] *= -1
+                predicted_3d_pos[1, :, joints_left + joints_right] = predicted_3d_pos[1, :, joints_right + joints_left]
+                predicted_3d_pos = torch.mean(predicted_3d_pos, dim=0, keepdim=True)
+
+            if return_predictions:
+                return predicted_3d_pos.squeeze(0).cpu().numpy()
+
+            inputs_3d = torch.from_numpy(batch.astype('float32'))
+            if torch.cuda.is_available():
+                inputs_3d = inputs_3d.cuda()
+            inputs_3d[:, :, 0] = 0
+            if test_generator.augment_enabled():
+                inputs_3d = inputs_3d[:1]
+
+            error = mpjpe(predicted_3d_pos, inputs_3d)
+            epoch_loss_3d_pos_scale += inputs_3d.shape[0] * inputs_3d.shape[1] * n_mpjpe(predicted_3d_pos,
+                                                                                         inputs_3d).item()
+
+            epoch_loss_3d_pos += inputs_3d.shape[0] * inputs_3d.shape[1] * error.item()
+            N += inputs_3d.shape[0] * inputs_3d.shape[1]
+
+            inputs = inputs_3d.cpu().numpy().reshape(-1, inputs_3d.shape[-2], inputs_3d.shape[-1])
+            predicted_3d_pos = predicted_3d_pos.cpu().numpy().reshape(-1, inputs_3d.shape[-2], inputs_3d.shape[-1])
+
+            epoch_loss_3d_pos_procrustes += inputs_3d.shape[0] * inputs_3d.shape[1] * p_mpjpe(predicted_3d_pos, inputs)
+
+            # Compute velocity error
+            epoch_loss_3d_vel += inputs_3d.shape[0] * inputs_3d.shape[1] * mean_velocity_error(predicted_3d_pos, inputs)
+
+    if action is None:
+        print('----------')
+    else:
+        print('----' + action + '----')
+    e1 = (epoch_loss_3d_pos / N) * 1000
+    e2 = (epoch_loss_3d_pos_procrustes / N) * 1000
+    e3 = (epoch_loss_3d_pos_scale / N) * 1000
+    ev = (epoch_loss_3d_vel / N) * 1000
+    print('Test time augmentation:', test_generator.augment_enabled())
+    print('Protocol #1 Error (MPJPE):', e1, 'mm')
+    print('Protocol #2 Error (P-MPJPE):', e2, 'mm')
+    print('Protocol #3 Error (N-MPJPE):', e3, 'mm')
+    print('Velocity Error (MPJVE):', ev, 'mm')
+    print('----------')
+
+    return e1, e2, e3, ev
+
+
+print('Rendering...')
+
+gen = UnchunkedGenerator(None, None, [input_keypoints],
+                         pad=pad, causal_shift=causal_shift, augment=args.test_time_augmentation,
+                         kps_left=kps_left, kps_right=kps_right, joints_left=joints_left, joints_right=joints_right)
+prediction = evaluate(gen, return_predictions=True)
+
+print('Exporting joint positions to', args.viz_export)
+# Predictions are in camera space
+np.save(args.viz_export, prediction)
